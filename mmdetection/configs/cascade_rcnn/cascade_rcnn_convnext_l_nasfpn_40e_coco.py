7e2c426915ac0677bf9177c7c260d7340df1e8dd
andantecode <kb721a@naver.com>
2024-10-13 17:37:28 +0900

    change import config in cascade rcnn with nasfpn

--- mmdetection/configs/cascade_rcnn/cascade_rcnn_convnext_l_nasfpn_40e_coco.py
+++ mmdetection/configs/cascade_rcnn/cascade_rcnn_convnext_l_nasfpn_40e_coco.py
@@ -34,19 +34,4 @@ model = dict(
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
-optimizer = dict(
-    _delete_=True,
-    constructor='LearningRateDecayOptimizerConstructor',
-    type='AdamW',
-    lr=0.0001,
-    betas=(0.9, 0.999),
-    weight_decay=0.05,
-    paramwise_cfg={
-        'decay_rate': 0.95,
-        'decay_type': 'layer_wise',
-        'num_layers': 6
-    })
-
-lr_config = dict(warmup_iters=1000, step=[27, 33])
-runner = dict(type='EpochBasedRunner', max_epochs=40)
 
