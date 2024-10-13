6ba9e3946ca3f2b2d7c94c06969314d0bfbe57a7
andantecode <kb721a@naver.com>
2024-10-13 17:07:04 +0900

    implements cascade rcnn with convnext_large backbone

---
+++ mmdetection/configs/_base_/schedules/schedule_40e.py
@@ -0,0 +1,23 @@
+# optimizer
+optimizer = dict(
+    _delete_=True,
+    constructor='LearningRateDecayOptimizerConstructor',
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg={
+        'decay_rate': 0.95,
+        'decay_type': 'layer_wise',
+        'num_layers': 6
+    })
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=0.001,
+    step=[27, 33])
+runner = dict(type='EpochBasedRunner', max_epochs=40)
